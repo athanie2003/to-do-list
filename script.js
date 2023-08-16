647272a5b93931b0b9848d8b13df9c2e1d74ba3d
Aaravan <aaravan2003@gmail.com>
2023-08-16 16:51:29 -0400

    Create delete button for each item

--- script.js
+++ script.js
@@ -9,39 +9,33 @@ let numOfItems = 0;
 // add to html
 body.appendChild(listContainer);
 
-// textBox.addEventListener('click', () => {
-//     if(textBox.value.length < 1){
-//         addBtn.disabled = true;
-//     }
-//     else{
-//         addBtn.disabled = false;
-//     }
-// })
-
 
 // buttons
 addBtn.addEventListener('click', () => {
     if(textBox.value != ''){
-        numOfItems++;
-        const div = document.createElement('div');
-        div.classList.add(`item${1}`);
-        const input = document.createElement('input');
-        input.type = 'checkbox';
-        const label = document.createElement('label');
-        label.innerText = textBox.value;
-        const removeBtn = document.createElement('button');
-        removeBtn.innerText = 'X';
-        removeBtn.classList.add('remove');
-        div.appendChild(input);
-        div.appendChild(label);
-        div.appendChild(removeBtn);
-        listContainer.appendChild(div);
-        textBox.value = '';
+        createItem();
     }
 });
 
-deleteBtns.forEach(btn, () => {
-    btn.addEventListener('click', (event) => {
-        console.log(event.target);
+
+
+function createItem(){
+    numOfItems++;
+    const div = document.createElement('div');
+    div.classList.add(`item${1}`);
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    const label = document.createElement('label');
+    label.innerText = textBox.value;
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'X';
+    removeBtn.classList.add('remove');
+    div.appendChild(input);
+    div.appendChild(label);
+    div.appendChild(removeBtn);
+    listContainer.appendChild(div);
+    textBox.value = '';
+    removeBtn.addEventListener('click', (event) => {
+        div.remove();
     });
-});
+}
