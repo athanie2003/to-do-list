66d0f5cdd5dca4b314c834c07f32530304b506a3
Aaravan <aaravan2003@gmail.com>
2023-08-17 19:25:30 -0400

    Make data save when page refreshes

--- script.js
+++ script.js
@@ -4,55 +4,51 @@ const listContainer = document.createElement('div');
 const textBox = document.querySelector('.text-box');
 const addBtn = document.querySelector('.add');
 const clearBtn = document.querySelector('.clear');
-let numOfItems = 0;
 
 // add to html
 body.appendChild(listContainer);
 
 // load items from localStorage when page loads
 window.addEventListener('load', () => {
-    const savedItems = JSON.parse(localStorage.getItem('items')) ||
+    const savedItems = JSON.parse(localStorage.getItem('items')) || [];
     savedItems.forEach(savedItem => {
         createItem(savedItem.label, savedItem.checked);
     });
-});
 
+    // Apply styles to labels if checked
+    listContainer.querySelectorAll('.item').forEach(div => {
+        const input = div.querySelector('input');
+        const label = div.querySelector('label');
+        if (input.checked) {
+            label.style.textDecoration = 'line-through';
+        }
+    });
+});
 
 // buttons
 addBtn.addEventListener('click', () => {
-    if(textBox.value != ''){
-        createItem();
+    if(textBox.value !== ''){
+        createItem(textBox.value, false);
     }
 });
 
 clearBtn.addEventListener('click', () => {
-    if(listContainer.hasChildNodes){
-        const items = listContainer.querySelectorAll('div');
-        items.forEach(item => {
-            item.remove();
-        });
+    while(listContainer.firstChild){
+        listContainer.removeChild(listContainer.firstChild);
     }
+    savedItems();
 });
 
 
 // functions
-// stores item in localStorage
-function savedItems(){
-    const items = Array.from(listContainer.children).map(div => ({
-        label: div.querySelector('label').innerText,
-        checked: div.querySelector('input').checked
-    }));
-    localStorage.setItem('items', JSON.stringify(items));
-}
-
-function createItem(){
-    // numOfItems++;
+function createItem(labelText, isChecked){
     const div = document.createElement('div');
     div.classList.add(`item`);
     const input = document.createElement('input');
     input.type = 'checkbox';
+    input.checked = isChecked;
     const label = document.createElement('label');
-    label.innerText = textBox.value;
+    label.innerText = labelText;
     const removeBtn = document.createElement('button');
     removeBtn.innerText = 'X';
     removeBtn.classList.add('remove');
@@ -67,6 +63,7 @@ function createItem(){
         div.remove();
         savedItems();
     });
+
     input.addEventListener('change', () => {
         if(input.checked){
             label.style.textDecoration = 'line-through';
@@ -77,4 +74,13 @@ function createItem(){
         savedItems();
     });
     savedItems();
+}
+
+// stores item in localStorage
+function savedItems(){
+    const items = Array.from(listContainer.children).map(div => ({
+        label: div.querySelector('label').innerText,
+        checked: div.querySelector('input').checked
+    }));
+    localStorage.setItem('items', JSON.stringify(items));
 }
