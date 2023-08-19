a414f5e0e1ed669ee1caebf40551655434d00c13
Aaravan <aaravan2003@gmail.com>
2023-08-19 19:15:36 -0400

    Remove edit button but allow items to be edited

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ const addBtn = document.querySelector('.add');
 const bottomBtns = document.createElement('div');
 bottomBtns.classList.add('bottom-btns');
 const editBtn = document.createElement('button');
-editBtn.innerText = 'Edit';
-editBtn.classList.add('edit');
 const clearBtn = document.createElement('button');
-clearBtn.innerText = 'Clear';
+clearBtn.innerText = 'Clear All';
 clearBtn.classList.add('clear');
 
 // add to html
 container.appendChild(listContainer);
 container.appendChild(bottomBtns);
-bottomBtns.appendChild(editBtn);
 bottomBtns.appendChild(clearBtn);
 
 // load items from localStorage when page loads
@@ -39,7 +36,6 @@ window.addEventListener('load', () => {
 // keys
 document.addEventListener('keydown', event => {
     const pressedKey = event.key;
-
     if(pressedKey == 'Enter'){
         if(textBox.value !== ''){
             createItem(textBox.value, false);
@@ -81,7 +77,7 @@ function createItem(labelText, isChecked){
     textBox.value = '';
 
     // buttons from items created within function
-    removeBtn.addEventListener('click', (event) => {
+    removeBtn.addEventListener('click', () => {
         div.remove();
         savedItems();
     });
@@ -95,10 +91,47 @@ function createItem(labelText, isChecked){
         }
         savedItems();
     });
+
+    label.addEventListener('click', () => {
+        if(!div.classList.contains('editing')){
+            div.classList.add('editing');
+            const currentText = label.innerText;
+            const inputBox = document.createElement('input');
+            inputBox.type = 'text';
+            inputBox.value = currentText;
+            inputBox.classList.add('edit-input');
+
+            // replace label with input for editing
+            div.replaceChild(inputBox, label);
+            inputBox.focus();
+
+            // save changes
+            inputBox.addEventListener('blur', () => {
+                if(inputBox.value){
+                    label.innerText = inputBox.value;
+                }
+                div.replaceChild(label, inputBox);
+                div.classList.remove('editing');
+                savedItems();
+            });
+
+            document.addEventListener('keydown', event => {
+                const pressedKey = event.key;
+                if(pressedKey == 'Enter'){
+                    if(inputBox.value){
+                        label.innerText = inputBox.value;
+                    }
+                    div.replaceChild(label, inputBox);
+                    div.classList.remove('editing');
+                    savedItems();
+                }
+            });
+        }
+    });
     savedItems();
 }
 
-// stores item in localStorage
+// store item in localStorage
 function savedItems(){
     const items = Array.from(listContainer.children).map(div => ({
         label: div.querySelector('label').innerText,
